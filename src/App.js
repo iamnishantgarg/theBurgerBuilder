c2244d9df9fc76400009da8f83a5654ac2682dea
Nishant garg <iamnishantgarg@gmail.com>
2020-06-22 19:15:47 +0530

    logout done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Orders from "./containers/Orders/Orders";
 import Checkout from "./containers/Checkout/Checkout";
 import { Route, Switch } from "react-router-dom";
 import Auth from "./containers/Auth/Auth";
+import Logout from "./containers/Auth/logout/Logout";
 class App extends Component {
   render() {
     return (
@@ -12,6 +13,7 @@ class App extends Component {
         <Switch>
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" exact component={Orders} />
+          <Route path="/logout" component={Logout} />
           <Route path="/auth" component={Auth} />
           <Route path="/" exact component={BurgerBuilder} />
         </Switch>
