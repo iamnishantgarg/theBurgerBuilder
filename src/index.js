0957fb0abc453ce3ec2859b21f26eb4bb5c1949e
Nishant garg <iamnishantgarg@gmail.com>
2020-09-26 12:33:34 +0530

    extra files deleted

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
 // import reducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
-import * as serviceWorker from "./serviceWorker";
+
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import authReducer from "./store/reducers/auth";
 
@@ -47,8 +47,3 @@ ReactDOM.render(
   <React.StrictMode>{app}</React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
